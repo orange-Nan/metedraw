d5b73cfb25d7628e6c9265134f727cbaf281d053
Nephele <71633656+orange-Nan@users.noreply.github.com>
2022-02-18 10:55:47 +0800

    Update xls和csv文件读写.py

--- 数据/数据处理方法/示例2_文件读写/xls和csv文件读写.py
+++ 数据/数据处理方法/示例2_文件读写/xls和csv文件读写.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#xls文件读写
 file1 = r'C:\Users\LULU\Desktop\2.xls'
 data1 = pd.read_excel(file1) 
 print(data1.keys())
@@ -10,14 +11,11 @@ print(data1.keys())   #输出data1的每一列的名称（变量名）：Index([
 print(data1['date']) #输出data1的某一列
 
 file2 = r'C:\Users\LULU\Desktop\3.xls'
-data1.to_excel(file2)
+data1.to_excel(file2) #将data1写入file2
 
+#csv文件读写
 file3 = r'C:\Users\LULU\Desktop\4.csv'
 data2 = pd.read_csv(file3)
 
 file4 = r'C:\Users\LULU\Desktop\5.csv'
 data1.to_csv(file4)
-
-
-
-
