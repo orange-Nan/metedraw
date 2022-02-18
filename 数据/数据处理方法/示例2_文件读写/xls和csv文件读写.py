154538368d552b7ed21633ecf27bfa520cb78749
Nephele <71633656+orange-Nan@users.noreply.github.com>
2022-02-18 10:58:24 +0800

    Update xls和csv文件读写.py

--- 数据/数据处理方法/示例2_文件读写/xls和csv文件读写.py
+++ 数据/数据处理方法/示例2_文件读写/xls和csv文件读写.py
@@ -2,20 +2,20 @@ import pandas as pd
 
 #xls文件读写
 file1 = r'C:\Users\LULU\Desktop\2.xls'
-data1 = pd.read_excel(file1)  #从file1(2.xls)读取data1
+data1 = pd.read_excel(file1)   #从file1(2.xls)读取data1
 print(data1.keys())
 
-print(data1)  #输出data1
-print(len(data1))  #输出data1的行数(5行)
+print(data1)   #输出data1
+print(len(data1))   #输出data1的行数(5行)
 print(data1.keys())   #输出data1的每一列的名称（变量名）：Index(['date', 'var1', 'var2'], dtype='object'
 print(data1['date'])   #输出data1的某一列
 
 file2 = r'C:\Users\LULU\Desktop\3.xls'
-data1.to_excel(file2) #将data1写入file2(3.xls)
+data1.to_excel(file2)   #将data1写入file2(3.xls)
 
 #csv文件读写
 file3 = r'C:\Users\LULU\Desktop\4.csv'
-data2 = pd.read_csv(file3)
+data2 = pd.read_csv(file3)   #从file3(4.csv)读取data2，后面的操作请读者自行完成
 
 file4 = r'C:\Users\LULU\Desktop\5.csv'
-data1.to_csv(file4) #将data1写入file4(5.csv)
+data1.to_csv(file4)   #将data1写入file4(5.csv)
